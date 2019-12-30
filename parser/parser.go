890e2babbfc692eaa19a2ae38bd4adb6778e31ac
Neoxelox <alex.rodriguez.navas@gmail.com>
2019-12-30 14:30:03 +0100

    Init parser and ast

---
+++ parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"github.com/Neoxelox/monkey-interpreter/ast"
+	"github.com/Neoxelox/monkey-interpreter/lexer"
+	"github.com/Neoxelox/monkey-interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
